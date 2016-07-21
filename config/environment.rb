b0f5d7cfa9a3ff97a2098a15b4cc4329592f1e2f
nidhirajkoti <rajkotiya_nidhi@network.elanco.com>
2016-07-21 20:29:56 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings={ 
+            address: "smtp-z1-nomx.lilly.com",
+            port: 25,
+            domain: "lilly.com",
+            authentication: "plain"
+             }
