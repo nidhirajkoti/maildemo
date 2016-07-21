2ffaf9912877b529fb52461915aa898fc6574200
nidhirajkoti <rajkotiya_nidhi@network.elanco.com>
2016-07-21 18:33:31 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings={ 
+            address: "smtp-z1-nomx.lilly.com",
+            port: 587,
+            domain: "lilly.com",
+            authentication: "plain"
+             }
+
 end
