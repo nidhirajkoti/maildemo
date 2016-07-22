c26de253bf1b2788ad89fe366de08d558ddca2f2
nidhirajkoti <rajkotiya_nidhi@network.elanco.com>
2016-07-22 12:56:22 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp 
 ActionMailer::Base.smtp_settings = { 
-            address : "smtp.gmail.com",
-            port : 587,
-            domain : "gmail.com",
-            authentication : "plain",
-            user_name : "testnidhi59@gmail.com",
-            password : "admin@12345"
+            :address => "smtp.gmail.com",
+            :port => 587,
+            :domain => "gmail.com",
+            :authentication => "plain",
+            :user_name => "testnidhi59@gmail.com",
+            :password => "admin@12345"
              }
